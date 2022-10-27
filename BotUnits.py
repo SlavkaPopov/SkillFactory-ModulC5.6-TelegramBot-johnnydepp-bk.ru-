b7514c2be3e4579a1f61c1a7b591766c315a7927
SlavkaPopov <114118077+SlavkaPopov@users.noreply.github.com>
2022-10-28 00:25:21 +0200

    Create BotUnits.py

---
+++ BotUnits.py
@@ -0,0 +1,11 @@
+TOKEN = '5435450596:AAHfGaH9D3yGeCD5UcbUs4uaGkrA3H9mWe4'
+
+APIKEY = '83a75e4810582348296be52eac2aa884'
+
+currency = {
+    'рубль': 'RUB',
+    'доллар': 'USD',
+    'евро': 'EUR',
+    'биткоин': 'BTC',
+    'эфириум': 'ETH'
+}
